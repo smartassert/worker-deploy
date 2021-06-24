606ea2e959ff587de2f2c12dd8c5d44cab3fae67
Jon Cram <webignition@gmail.com>
2021-06-24 17:11:11 +0100

    Remove application code

--- config/bundles.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
-];
