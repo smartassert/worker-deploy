e8fd09599823ead2ffe0a37e2b26bb997f455a6e
Jon Cram <webignition@gmail.com>
2021-06-30 12:19:39 +0100

    Call ::set-output only if image-version is present (#51)

--- ci/read-image-versions.sh
+++ ci/read-image-versions.sh
@@ -5,7 +5,9 @@ cat local.image-versions.env
 
 while read line
 do
-  key=$(echo $line | cut -d'=' -f1)
-  value=$(echo $line | cut -d'=' -f2)
-  echo "::set-output name=$key::$value"
+  if [ "" != "$line" ]; then
+    key=$(echo $line | cut -d'=' -f1)
+    value=$(echo $line | cut -d'=' -f2)
+    echo "::set-output name=$key::$value"
+  fi
 done < local.image-versions.env
