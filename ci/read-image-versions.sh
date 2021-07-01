b652373090229e6b8c8c869e261d94648a7eca9c
Jon Cram <webignition@gmail.com>
2021-07-01 16:17:56 +0100

    Parameterise path to image versions when reading (#63)

--- ci/read-image-versions.sh
+++ ci/read-image-versions.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-ls -la
-cat local.image-versions.env
-
 while read line
 do
   if [ "" != "$line" ]; then
@@ -10,4 +7,4 @@ do
     value=$(echo $line | cut -d'=' -f2)
     echo "::set-output name=$key::$value"
   fi
-done < local.image-versions.env
+done < $IMAGE_VERSIONS_PATH
