50282824aba4fb044c3d9e6aab92e7d638e98148
Jon Cram <webignition@gmail.com>
2021-07-05 09:53:47 +0100

    Fix creation of snapshot name for automatic releases (#74)

--- ci/create-snapshot-name.sh
+++ ci/create-snapshot-name.sh
@@ -9,7 +9,10 @@ EXIT_CODE_INVALID_EVENT=1
 # EVENT_NAME='pull_request' PR_NUMBER=101 ./create-snapshot-name.sh
 
 # publish release
-# EVENT_NAME='release' RELEASE_TAG_NAME='"0.1"' ./create-snapshot-name.sh
+# EVENT_NAME='release' RELEASE_TAG_NAME='0.1' ./create-snapshot-name.sh
+
+# repository_dispatch for worker-release
+# EVENT_NAME='repository_dispatch' RELEASE_TAG_NAME='0.4.12' ./create-snapshot-name.sh
 
 TIMESTAMP=$(date --utc +%Y-%m-%dT%H:%M:%SZ)
 
@@ -18,6 +21,8 @@ if [ $EVENT_NAME = "push" ]; then
 elif [ $EVENT_NAME = "pull_request" ]; then
   SUFFIX="pull-request-${PR_NUMBER}"
 elif [ $EVENT_NAME = "release" ]; then
+  SUFFIX="release-$(echo $RELEASE_TAG_NAME | tr -d '"')"
+elif [ $EVENT_NAME = "repository_dispatch" ]; then
   SUFFIX="release-$(echo $RELEASE_TAG_NAME | tr -d '"')"
 else
   exit $EXIT_CODE_INVALID_EVENT
