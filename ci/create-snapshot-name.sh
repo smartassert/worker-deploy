daaa57784fa01af91b77c25155127637a31dedec
Jon Cram <webignition@gmail.com>
2021-08-12 11:37:38 +0100

    Move version to job-level env var

--- ci/create-snapshot-name.sh
+++ ci/create-snapshot-name.sh
@@ -9,17 +9,17 @@ EXIT_CODE_INVALID_EVENT=1
 # EVENT_NAME='pull_request' PR_NUMBER=101 ./create-snapshot-name.sh
 
 # publish release
-# EVENT_NAME='release' RELEASE_TAG_NAME='0.1' ./create-snapshot-name.sh
+# EVENT_NAME='release' VERSION='0.1' ./create-snapshot-name.sh
 
 # repository_dispatch for worker-release
-# EVENT_NAME='repository_dispatch' RELEASE_TAG_NAME='0.4.12' ./create-snapshot-name.sh
+# EVENT_NAME='repository_dispatch' VERSION='0.4.12' ./create-snapshot-name.sh
 
 if [ $EVENT_NAME = "push" ]; then
-  echo "master"
+  echo $VERSION
 elif [ $EVENT_NAME = "pull_request" ]; then
   echo "pull-request-${PR_NUMBER}"
 elif [ $EVENT_NAME = "release" ] || [ $EVENT_NAME = "repository_dispatch" ]; then
-  echo "release-$(echo $RELEASE_TAG_NAME | tr -d '"')"
+  echo "release-$(echo $VERSION | tr -d '"')"
 else
   exit $EXIT_CODE_INVALID_EVENT
 fi
