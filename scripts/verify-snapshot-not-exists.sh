621bf670fb090c6819d0343ad43ebe450d5abf8b
Jon Cram <webignition@gmail.com>
2021-08-23 15:33:33 +0100

    Fix issues with verify-snapshot-not-exists.sh

--- scripts/verify-snapshot-not-exists.sh
+++ scripts/verify-snapshot-not-exists.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 EXIT_CODE_RESPONSE_ID_MISSING=1
-EXIT_CODE_SNAPSHOT_ID_MISSING=2
 EXIT_CODE_RESPONSE_ID_INCORRECT=3
 
 AUTH_HEADER="Authorization: Bearer ${DIGITALOCEAN_API_TOKEN}"
@@ -9,15 +8,15 @@ URL="https://api.digitalocean.com/v2/snapshots/${IMAGE_ID}"
 
 RESPONSE_JSON=$(curl -s -X GET -H 'Content-Type: application/json' -H "${AUTH_HEADER}" "${URL}")
 
-RESPONSE_JSON_HAS_ID=$(echo $RESPONSE_JSON | jq 'has("id")')
-if [ $RESPONSE_JSON_HAS_ID != "true" ]; then
+RESPONSE_JSON_HAS_ID=$(echo "$RESPONSE_JSON" | jq 'has("id")')
+if [ "$RESPONSE_JSON_HAS_ID" != "true" ]; then
   exit $EXIT_CODE_RESPONSE_ID_MISSING
 fi
 
-RESPONSE_ID=$(echo $RESPONSE_JSON | jq '.id')
+RESPONSE_ID=$(echo "$RESPONSE_JSON" | jq '.id')
 EXPECTED_RESPONSE_ID="\"not_found\""
 
-if [ $RESPONSE_ID != $EXPECTED_RESPONSE_ID ]; then
+if [ "$RESPONSE_ID" != "$EXPECTED_RESPONSE_ID" ]; then
   exit $EXIT_CODE_RESPONSE_ID_INCORRECT
 fi
 
