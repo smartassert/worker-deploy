4a32ea9fa41b43cc4281bf3a202947ee7ba1a322
Jon Cram <webignition@gmail.com>
2021-08-24 15:12:12 +0100

    (temp) intentionally fail in packer build script

--- scripts/packer-build.sh
+++ scripts/packer-build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-packer build "$IMAGE_DEFINITION" | tee packer.log
-PACKER_LOG_CONTENTS=$(cat packer.log)
-IMAGE_ID=$(echo "$PACKER_LOG_CONTENTS" | tail -1 | grep -P -o 'ID: \d+' | tr -d 'ID: ')
+#packer build "$IMAGE_DEFINITION" | tee packer.log
+#PACKER_LOG_CONTENTS=$(cat packer.log)
+#IMAGE_ID=$(echo "$PACKER_LOG_CONTENTS" | tail -1 | grep -P -o 'ID: \d+' | tr -d 'ID: ')
+
+IMAGE_ID=foo
+PACKER_LOG_CONTENTS="log content goes here"
 
 if ! [[ $IMAGE_ID =~ ^[0-9]+$ ]] ; then
    echo "$PACKER_LOG_CONTENTS"
