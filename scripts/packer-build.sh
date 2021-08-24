fa290cc5855eac186a1d9dbbd8494f0d9e0b6103
Jon Cram <webignition@gmail.com>
2021-08-24 14:51:06 +0100

    Factor out release.create-image packer image creation logic to script

---
+++ scripts/packer-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+packer build "$IMAGE_DEFINITION" | tee packer.log
+PACKER_LOG_CONTENTS=$(cat packer.log)
+IMAGE_ID=$(echo "$PACKER_LOG_CONTENTS" | tail -1 | grep -P -o 'ID: \d+' | tr -d 'ID: ')
+
+if ! [[ $IMAGE_ID =~ ^[0-9]+$ ]] ; then
+   echo "$PACKER_LOG_CONTENTS"
+   exit 1
+fi
+
+echo "$IMAGE_ID"
