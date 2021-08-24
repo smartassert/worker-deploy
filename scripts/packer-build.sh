db321ce3efa88e7818eefca2e8b3f16e007253ec
Jon Cram <webignition@gmail.com>
2021-08-24 15:25:08 +0100

    Export image ID in packer build script

--- scripts/packer-build.sh
+++ scripts/packer-build.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-#packer build "$IMAGE_DEFINITION" | tee packer.log
-#PACKER_LOG_CONTENTS=$(cat packer.log)
-#IMAGE_ID=$(echo "$PACKER_LOG_CONTENTS" | tail -1 | grep -P -o 'ID: \d+' | tr -d 'ID: ')
-
-IMAGE_ID=foo
-PACKER_LOG_CONTENTS="log content goes here"
+packer build "$IMAGE_DEFINITION" | tee packer.log
+IMAGE_ID=$(cat packer.log | tail -1 | grep -P -o 'ID: \d+' | tr -d 'ID: ')
 
 if ! [[ $IMAGE_ID =~ ^[0-9]+$ ]] ; then
-   echo "$PACKER_LOG_CONTENTS"
    exit 1
 fi
 
-echo "$IMAGE_ID"
+export IMAGE_ID
