43957d8575e153dbdd7d7e4128218856f3fe973a
Jon Cram <webignition@gmail.com>
2021-08-24 14:51:18 +0100

    Combine logic for outputting image versions and creating release notes (#154)

--- scripts/create-release-notes.sh
+++ scripts/create-release-notes.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-rm -f "$RELEASE_NOTES_PATH"
-
-{
-  echo "- compiler: $COMPILER_VERSION"
-  echo "- chrome runner: $CHROME_RUNNER_VERSION"
-  echo "- firefox runner: $FIREFOX_RUNNER_VERSION"
-  echo "- delegator: $DELEGATOR_VERSION"
-  echo "- worker: $WORKER_VERSION"
-} >> "$RELEASE_NOTES_PATH"
+echo "- compiler: $COMPILER_VERSION"
+echo "- chrome runner: $CHROME_RUNNER_VERSION"
+echo "- firefox runner: $FIREFOX_RUNNER_VERSION"
+echo "- delegator: $DELEGATOR_VERSION"
+echo "- worker: $WORKER_VERSION"
