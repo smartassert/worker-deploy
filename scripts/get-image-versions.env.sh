5fa75a24f7c13a20360a717786fa56cccd850b9e
Jon Cram <webignition@gmail.com>
2021-08-25 16:13:28 +0100

    Fix shellcheck linting

--- scripts/get-image-versions.env.sh
+++ scripts/get-image-versions.env.sh
@@ -2,10 +2,10 @@
 
 URL="https://raw.githubusercontent.com/smartassert/worker-deploy/$VERSION/image-versions.env"
 
-STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" $URL)
+STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
 
 if [ "200" = "$STATUS_CODE" ]; then
-  curl -s -o image-versions.env $URL
+  curl -s -o image-versions.env "$URL"
 else
   exit 1
 fi
