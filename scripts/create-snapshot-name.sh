24d9da30a1cc3b38d76c2993e73e325a1e963522
Jon Cram <webignition@gmail.com>
2021-08-25 16:45:49 +0100

    Create snapshot name for workflow_dispatch event

--- scripts/create-snapshot-name.sh
+++ scripts/create-snapshot-name.sh
@@ -11,14 +11,14 @@ EXIT_CODE_INVALID_EVENT=1
 # publish release
 # EVENT_NAME='release' VERSION='0.1' ./create-snapshot-name.sh
 
-# repository_dispatch for worker-release
-# EVENT_NAME='repository_dispatch' VERSION='0.4.12' ./create-snapshot-name.sh
+# workflow_dispatch
+# EVENT_NAME='workflow_dispatch' VERSION='0.4.12' ./create-snapshot-name.sh
 
 if [ "$EVENT_NAME" = "push" ]; then
   echo "$VERSION"
 elif [ "$EVENT_NAME" = "pull_request" ]; then
   echo "pull-request-${PR_NUMBER}"
-elif [ "$EVENT_NAME" = "release" ] || [ "$EVENT_NAME" = "repository_dispatch" ]; then
+elif [ "$EVENT_NAME" = "release" ] || [ "$EVENT_NAME" = "workflow_dispatch" ]; then
   echo "release-$(echo "$VERSION" | tr -d '"')"
 else
   exit $EXIT_CODE_INVALID_EVENT
