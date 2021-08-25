8a214338d1d10138294358f1641af164a64a5e21
Jon Cram <webignition@gmail.com>
2021-08-25 16:21:36 +0100

    Output url and status code if status code is not 200

--- scripts/get-image-versions-env.sh
+++ scripts/get-image-versions-env.sh
@@ -4,8 +4,10 @@ URL="https://raw.githubusercontent.com/smartassert/worker-deploy/$VERSION/image-
 
 STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
 
-if [ "200" = "$STATUS_CODE" ]; then
-  curl -s -o image-versions.env "$URL"
-else
+if [ "200" != "$STATUS_CODE" ]; then
+  echo "URL: $URL"
+  echo "Status code: $STATUS_CODE"
   exit 1
 fi
+
+curl -s -o image-versions.env "$URL"
