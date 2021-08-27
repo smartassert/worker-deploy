d08a87d81314516722280d9712d4510419dad286
Jon Cram <webignition@gmail.com>
2021-08-27 10:52:32 +0100

    Output packer image id to file for subsequent access

--- scripts/packer-build.sh
+++ scripts/packer-build.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-PACKER_OUTPUT=$(packer build "$IMAGE_DEFINITION" | tee packer.log)
-IMAGE_ID=$(echo "$PACKER_OUTPUT" | tail -1 | grep -P -o 'ID: \d+' | tr -d 'ID: ')
+packer build "$IMAGE_DEFINITION" | tee "$PACKER_LOG_PATH"
+IMAGE_ID=$(tail -1 "$PACKER_LOG_PATH" | grep -P -o 'ID: \d+' | tr -d 'ID: ')
 
 if ! [[ $IMAGE_ID =~ ^[0-9]+$ ]] ; then
-  echo "$PACKER_OUTPUT"
   exit 1
 fi
 
-echo "$IMAGE_ID"
+echo "$IMAGE_ID" > $IMAGE_ID_PATH
