be5e041ed4899f3d24262f94ec03539a388b5161
Jon Cram <webignition@gmail.com>
2021-08-27 12:49:30 +0100

    Output image ID to file in packer-build.sh, don't export (#184)

--- scripts/packer-build.sh
+++ scripts/packer-build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-packer build "$IMAGE_DEFINITION" | tee packer.log
-IMAGE_ID=$(tail -1 packer.log | grep -P -o 'ID: \d+' | tr -d 'ID: ')
+packer build "$IMAGE_DEFINITION" | tee "$PACKER_LOG_PATH"
+IMAGE_ID=$(tail -1 "$PACKER_LOG_PATH" | grep -P -o 'ID: \d+' | tr -d 'ID: ')
 
 if ! [[ $IMAGE_ID =~ ^[0-9]+$ ]] ; then
-   exit 1
+  exit 1
 fi
 
-export IMAGE_ID
+echo "$IMAGE_ID" > "$IMAGE_ID_PATH"
