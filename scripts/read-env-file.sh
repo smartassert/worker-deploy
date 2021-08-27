2ab26879b1613f89f5737dde06310abd252d6bcd
Jon Cram <webignition@gmail.com>
2021-08-27 15:34:27 +0100

    Provide output template to read-env-vars.sh (#188)

--- scripts/read-env-file.sh
+++ scripts/read-env-file.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+if [ -z "$OUTPUT_TEMPLATE" ]; then
+  OUTPUT_TEMPLATE="_key_=_value_"
+fi
+
 while read -r line
 do
   if [ "" != "$line" ]; then
     key=$(echo "$line" | cut -d'=' -f1)
     value=$(echo "$line" | cut -d'=' -f2)
-    echo "::set-output name=$key::$value"
+
+    output_line="$OUTPUT_TEMPLATE"
+
+    output_line="${output_line//_key_/$key}"
+    output_line="${output_line//_value_/$value}"
+
+    echo "$output_line"
   fi
 done < "$ENV_FILE_PATH"
--- scripts/read-image-versions-env.sh
+++ scripts/read-image-versions-env.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-ENV_FILE_PATH=image-versions.env "$(dirname "$0")/read-env-file.sh"
+ENV_FILE_PATH=image-versions.env \
+OUTPUT_TEMPLATE="::set-output name=_key_::_value_" \
+"$(dirname "$0")/read-env-file.sh"
