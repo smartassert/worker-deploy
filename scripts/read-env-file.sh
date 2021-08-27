369d15cf81c10324ea8be3ec22d54fe98718dd77
Jon Cram <webignition@gmail.com>
2021-08-27 15:16:40 +0100

    Provide output template to read-env-vars.sh

--- scripts/read-env-file.sh
+++ scripts/read-env-file.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+if [ -z "$OUTPUT_TEMPLATE" ]; then
+  OUTPUT_TEMPLATE="{{ key }}={{ value }}"
+fi
+
 while read -r line
 do
   if [ "" != "$line" ]; then
     key=$(echo "$line" | cut -d'=' -f1)
     value=$(echo "$line" | cut -d'=' -f2)
-    echo "::set-output name=$key::$value"
+
+    output_line="$OUTPUT_TEMPLATE"
+
+    output_line=$(echo "$output_line" | sed "s/{{ key }}/${key}/")
+    output_line=$(echo "$output_line" | sed "s/{{ value }}/${value}/")
+
+    echo "$output_line"
   fi
 done < "$ENV_FILE_PATH"
