a18396b27a299674226e0009394d8f1dd3c6f3e4
Jon Cram <webignition@gmail.com>
2021-08-31 09:57:03 +0100

    Unify verify-snapshot-*exists.sh (#190)

---
+++ scripts/verify-snapshot-existence.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ -z "$EXPECTED_EXISTS" ] || { [ "$EXPECTED_EXISTS" != "true" ] && [ "$EXPECTED_EXISTS" != "false" ]; }; then
+  EXPECTED_EXISTS="true"
+fi
+
+AUTH_HEADER="Authorization: Bearer ${DIGITALOCEAN_API_TOKEN}"
+URL="https://api.digitalocean.com/v2/snapshots/${IMAGE_ID}"
+
+RESPONSE_JSON=$(curl -s -X GET -H 'Content-Type: application/json' -H "${AUTH_HEADER}" "${URL}")
+RESPONSE_JSON_HAS_SNAPSHOT=$(echo "$RESPONSE_JSON" | jq 'has("snapshot")')
+
+exit "$([ "$RESPONSE_JSON_HAS_SNAPSHOT" = "$EXPECTED_EXISTS" ] && echo 0 || echo 1)"
