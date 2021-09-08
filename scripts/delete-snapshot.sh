42d52341135456caf431c8746222a150d50a01f1
Jon Cram <webignition@gmail.com>
2021-09-08 17:36:11 +0100

    Use delete

--- scripts/delete-snapshot.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-AUTH_HEADER="Authorization: Bearer ${DIGITALOCEAN_API_TOKEN}"
-URL="https://api.digitalocean.com/v2/snapshots/${IMAGE_ID}"
-
-curl -s -X DELETE -H 'Content-Type: application/json' -H "${AUTH_HEADER}" "${URL}"
