9723ebeaa524c649b844a0a47e33a77626019f8c
Jon Cram <webignition@gmail.com>
2021-10-26 14:18:37 +0100

    Update installation of docker-ce, docker-ce-cli, containerd.io (#248)

--- provision.sh
+++ provision.sh
@@ -13,16 +13,16 @@ run_command_until_successful sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
+    gnupg \
     gnupg-agent \
+    lsb-release \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 run_command_until_successful sudo apt-get update
 run_command_until_successful sudo apt-get install -y \
