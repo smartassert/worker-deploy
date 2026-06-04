0a02912181bb83ce117b6dbd61b57db9095429de
Jon Cram <webignition@gmail.com>
2026-06-04 10:56:39 +0100

    Remove superfluous db table content removal after self test

--- self-test/app.sh
+++ self-test/app.sh
@@ -63,30 +63,6 @@ sudo \
   WORKER_VERSION="$WORKER_VERSION" \
   docker compose up -d --remove-orphans
 
-DB_TABLES=(
-  "job"
-  "test"
-  "worker_event"
-  "worker_event_reference"
-  "worker_event_worker_event_reference"
-  "source"
-)
-
-for TABLE in "${DB_TABLES[@]}"
-  do
-    echo "Removing all from $TABLE"
-
-    sudo \
-      LOCAL_SOURCE_PATH="$LOCAL_SOURCE_PATH" \
-      COMPILER_VERSION="$COMPILER_VERSION" \
-      CHROME_RUNNER_VERSION="$CHROME_RUNNER_VERSION" \
-      FIREFOX_RUNNER_VERSION="$FIREFOX_RUNNER_VERSION" \
-      DELEGATOR_VERSION="$DELEGATOR_VERSION" \
-      WORKER_VERSION="$WORKER_VERSION" \
-      docker compose exec -T -e PGPASSWORD=password! postgres psql -U postgres -d worker-db -c "DELETE FROM ${TABLE}"
-
-  done
-
 sudo apt-get -qq -y remove php8.1-cli php8.1-curl php8.1-dom php8.1-mbstring zip > /dev/null
 sudo apt-get -qq -y autoremove > /dev/null
 sudo rm -Rf ./self-test
